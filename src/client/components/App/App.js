a10c8774c574648c2eb510c3fa600604d10db6db
Miles Kang <milesjkang@gmail.com>
2020-07-13 14:53:43 -0700

    Stack framework.

---
+++ src/client/components/App/App.js
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import axios from 'axios';
+
+import theme from '@instructure/canvas-high-contrast-theme';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      nothing
+    </div>
+  );
+};
+
+export default App;
