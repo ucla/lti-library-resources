f00f84d8c59102f2dd479e117c932a7989a3e5af
finnzink <finnzink1@gmail.com>
2020-07-23 08:58:04 -0700

    jest tests added

--- scripts/get_reserves.js
+++ scripts/get_reserves.js
@@ -1,74 +1,81 @@
-const got = require('got'); 
-const MongoClient = require('mongodb').MongoClient;
-var parseString = require('xml2js').parseString;
-const registrar = require("./registrar"); 
-require('dotenv').config(); 
+const got = require('got');
+const { MongoClient } = require('mongodb');
+const { parseString } = require('xml2js');
+const registrar = require('./registrar');
+require('dotenv').config();
 
 // Mongo values
-const mongourl = 'mongodb://localhost:27017';
-const dbName = 'course_reserves';
+const mongourl = process.env.MONGO_URL;
+const dbName = process.env.DB_NAME;
 
-const client = new MongoClient(mongourl);
+const client = new MongoClient(mongourl, { useUnifiedTopology: true });
 
 // test with 20S - URLs are empty though
 const args = process.argv.slice(2);
-if (args.length != 1) {
-    console.log("Usage: node script.js TERM");
+if (args.length !== 1) {
+  console.log('ERROR: Bad arguments. Usage: node script.js TERM');
+  process.exit();
 }
-let term = args[0];  
+const term = args[0];
 
 (async () => {
-	try {
-        let DBentries = []; 
+  try {
+    // connect to mongodb
+    await client.connect();
+    console.log('Connected correctly to mongodb server');
+    const db = client.db(dbName);
 
-        // get token to query registrar
-        const token = await registrar.newToken(); 
-        console.log(token); 
+    // get dept IDs which have course reserves
+    const response = await got(
+      `https://webservices.library.ucla.edu/reserves/departments/during/${term}`
+    );
+    let deptIDs = [];
+    parseString(response.body, function(err, result) {
+      const {
+        departmentList: { department: depts },
+      } = result;
+      deptIDs = depts.map(dept => dept.departmentID[0]);
+    });
 
-        // connect to mongodb 
-        await client.connect();
-        console.log("Connected correctly to mongodb server");
-        const db = client.db(dbName);
-        
-        // get dept IDs which have course reserves
-        const response = await got('https://webservices.library.ucla.edu/reserves/departments/during/' + term);
-        let dept_IDs = [];
-        parseString(response.body, function (err, result) {
-            const {departmentList: {department: depts}} = result; 
-            dept_IDs = depts.map(dept => dept.departmentID[0]); 
-        }); 
+    // get courses from the department IDs
+    const numDepts = deptIDs.length;
+    for (let i = 0; i < numDepts; i += 1) {
+      // process.stdout.clearLine();
+      // process.stdout.cursorTo(0);
+      // const str = (((i + 1)/num_depts * 100).toPrecision(3).toString() + "% complete");
+      // process.stdout.write(str);
+      const deptResponse = await got(
+        `https://webservices.library.ucla.edu/reserves/courses/dept/${deptIDs[i]}/term/${term}`
+      );
 
-        // get courses from the department IDs 
-        const num_depts = dept_IDs.length; 
-        for (let i = 0; i < num_depts; i++) {
-            //process.stdout.clearLine();
-            //process.stdout.cursorTo(0);
-            //const str = (((i + 1)/num_depts * 100).toPrecision(3).toString() + "% complete"); 
-            //process.stdout.write(str);
-            const dept_response = await got('https://webservices.library.ucla.edu/reserves/courses/dept/' 
-                + dept_IDs[i] + '/term/' + term);
+      let courses = [];
+      parseString(deptResponse.body, function(err, result) {
+        const {
+          courseList: { course: coursesObj },
+        } = result;
+        courses = coursesObj;
+      });
 
-            let courses = []; 
-            parseString(dept_response.body, function (err, result) { 
-                const {courseList: {course: courses_obj}} = result; 
-                courses = courses_obj; 
-            });
-
-            for (let j = 0; j < courses.length; j++) {
-                const srs = courses[j].srsNumber[0];
-                const url = courses[j].url[0]; 
-                const shortname = await registrar.getShortname(token, term, srs);  
-                if (shortname != "-1") {
-                    exists = await db.collection('reserves').countDocuments({"shortname": shortname}, { limit: 1 });   
-                    if (exists == 0) {
-                        await db.collection('reserves').insertOne({url: url, shortname: shortname});
-                    }
-                }     
-            }
-        } 
-        client.close(); 
-	} catch (error) {
-		console.log(error);
-	}
+      for (let j = 0; j < courses.length; j += 1) {
+        const srs = courses[j].srsNumber[0];
+        const url = courses[j].url[0];
+        const shortname = await registrar.getShortname(
+          `${process.env.REGISTRAR_API_URL}/sis/api/v1/Dictionary/${term}/${srs}/CourseClassIdentifiers`
+        );
+        console.log(`${srs}: ${shortname}`);
+        if (shortname !== null) {
+          const exists = await db
+            .collection('reserves')
+            .countDocuments({ shortname }, { limit: 1 });
+          if (exists === 0) {
+            await db.collection('reserves').insertOne({ url, shortname });
+          }
+        }
+      }
+    }
+    client.close();
+  } catch (error) {
+    console.log(error);
+  }
 })();
-// console.log(util.inspect(dept_IDs, false, null, true)); 
+// console.log(util.inspect(dept_IDs, false, null, true));
--- scripts/registrar.js
+++ scripts/registrar.js
@@ -1,66 +1,126 @@
-var axios = require('axios');
-var https = require('https'); 
-var fs = require('fs'); 
+const axios = require('axios');
+const https = require('https');
+const fs = require('fs');
 require('dotenv').config();
 
-var data = 'grant_type=client_credentials';
-const certFile = process.env.CERT_NAME; 
+const data = 'grant_type=client_credentials';
+const certFile = process.env.CERT_NAME;
 const keyFile = process.env.PRIVATE_KEY;
 
 const httpsAgent = new https.Agent({
-    cert: fs.readFileSync(certFile),
-    key: fs.readFileSync(keyFile), 
-  }); 
+  cert: fs.readFileSync(certFile),
+  key: fs.readFileSync(keyFile),
+});
 
-var config = {
+const tokenConfig = {
   method: 'post',
-  url: 'https://webservicesqa.it.ucla.edu:4443/oauth2/token',
-  headers: { 
-    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+  url: `${process.env.REGISTRAR_API_URL}:4443/oauth2/token`,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
   },
   auth: {
     username: process.env.CLIENT_ID,
-    password: process.env.SECRET
+    password: process.env.SECRET,
   },
-  data : data,
-  httpsAgent: httpsAgent
+  data,
+  httpsAgent,
 };
 
-const registrar = {
-    async newToken() {
-        try {
-            const response = await axios(config, httpsAgent); 
-            const {access_token: token} = response.data; 
-            console.log("token generated. ");
-            return token; 
-        } catch (error) {
-            console.log(error); 
-        }
-    },
-    async getShortname(token, term, srs) {
-        try {
-            var config = {
-                method: 'get',
-                url: ('https://webservicesqa.it.ucla.edu/sis/api/v1/Dictionary/' + term + '/' 
-                    + srs + '/CourseClassIdentifiers'),
-                headers: { 
-                  'esmAuthnClientToken': token
-                }
-              };
-            const response = await axios(config); 
-            if (response.status == 200)
-            {
-                const {courseClassIdentifiers: [{courseClassIdentifierCollection:[
-                    {subjectAreaCode:subArea, courseCatalogNumber:catNum, classSectionNumber:secNum}]}]} = response.data; 
-                const updateCatNum = (catNum.charAt(catNum.length - 1) == 'M') ? ('M' + catNum.replace(/\s|M|^0+/g, '')) : (catNum.replace(/\s|^0+/g, ''));
-                const shortname = (term + '-' + subArea.replace(/\s|&/g, '') + updateCatNum + '-' + secNum.replace(/^0+/g, '')); 
-                console.log(shortname); 
-                return shortname;  
-            }
-        } catch (error) {
-            return("-1");  
-        }
+async function getToken() {
+  try {
+    const response = await axios(tokenConfig, httpsAgent);
+    const { access_token: token } = response.data;
+    return token;
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function cacheToken(token) {
+  try {
+    process.env.reg_token = token;
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function refreshToken() {
+  try {
+    const token = await registrar.getToken();
+    await registrar.cacheToken(token);
+    return token;
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function call(url, method) {
+  try {
+    const config = {
+      method,
+      url,
+      headers: {
+        esmAuthnClientToken:
+          process.env.reg_token === undefined
+            ? await registrar.refreshToken()
+            : process.env.reg_token,
+      },
+    };
+    const response = await axios(config);
+    return response.data;
+  } catch (error) {
+    if (error.response.status === 401 || error.response.status === 403) {
+      // Token may have expired
+      const config = {
+        method,
+        url,
+        headers: {
+          esmAuthnClientToken: await registrar.refreshToken(),
+        },
+      };
+      const retryResponse = await axios(config);
+      if (retryResponse.status === 200) {
+        return retryResponse.data;
+      }
     }
-}; 
+    return null;
+  }
+}
+async function getShortname(url) {
+  try {
+    const response = await registrar.call(url, 'get');
+    if (response === null) return null;
+    const {
+      courseClassIdentifiers: [
+        {
+          courseClassIdentifierCollection: [
+            {
+              subjectAreaCode: subArea,
+              courseCatalogNumber: catNum,
+              classSectionNumber: secNum,
+            },
+          ],
+        },
+      ],
+    } = response;
+    const updateCatNum =
+      catNum.charAt(catNum.length - 1) === 'M'
+        ? `M${catNum.replace(/\s|M|^0+/g, '')}`
+        : catNum.replace(/\s|^0+/g, '');
+    const shortname = `${'20S'}-${subArea.replace(
+      /\s|&/g,
+      ''
+    )}${updateCatNum}-${secNum.replace(/^0+/g, '')}`;
+    return shortname;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+const registrar = {
+  getShortname,
+  call,
+  getToken,
+  cacheToken,
+  refreshToken,
+};
 
 module.exports = registrar;
